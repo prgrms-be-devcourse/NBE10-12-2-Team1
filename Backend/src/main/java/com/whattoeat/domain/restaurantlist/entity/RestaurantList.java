221fdcf5f1bf7015df43d63433be09075178a3cd
boramH <ky26212621@gmail.com>
2026-06-26 23:42:24 +0900

    fix: RestaurantList 안쓰는 import 제거(#33)

--- Backend/src/main/java/com/whattoeat/domain/restaurantlist/entity/RestaurantList.java
+++ Backend/src/main/java/com/whattoeat/domain/restaurantlist/entity/RestaurantList.java
@@ -5,7 +5,6 @@ import com.whattoeat.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
