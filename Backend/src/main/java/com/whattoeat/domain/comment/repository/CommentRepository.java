650beeb99a015125bcc1b5544f521b9ca7732fa3
evelman2001-lgtm <evelman2001@gmail.com>
2026-06-26 14:03:55 +0900

    [chore] Redis 설정 구현 (#32)
    * chore: 프로젝트 초기 설정(#1)
    
    * chore: 초기설정(#9)
    
    * feat: Comment엔티티 생성(#14)
    
    * chore: .idea 디렉토리 git 추적 제거(#24)
    
    * chore: Redis 설정구현(#24)
    
    * chore: Redis 설정구현(#24)

--- Backend/src/main/java/com/whattoeat/domain/comment/repository/CommentRepository.java
+++ Backend/src/main/java/com/whattoeat/domain/comment/repository/CommentRepository.java
@@ -1,7 +1,6 @@
 package com.whattoeat.domain.comment.repository;
 
 import com.whattoeat.domain.comment.entity.Comment;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
