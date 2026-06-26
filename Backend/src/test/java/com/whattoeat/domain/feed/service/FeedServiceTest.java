b5988afdbaa6fceb1dd4bd86922ed5e71f7a843f
SOL4R1S <mingh662@gmail.com>
2026-06-26 17:08:36 +0900

    fix: comment 엔티티 manytoone 수정(#31)

--- Backend/src/test/java/com/whattoeat/domain/feed/service/FeedServiceTest.java
+++ Backend/src/test/java/com/whattoeat/domain/feed/service/FeedServiceTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.mockito.ArgumentMatchers.any;
 
