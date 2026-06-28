3c38bf36b9dae2f23cd37908c794072d00c0e1de
Boram-Hwang <95403102+Boram-Hwang@users.noreply.github.com>
2026-06-29 08:57:55 +0900

    [feat] RestaurantListItem 엔티티 및 리포지터리 추가 (#43)
    * fix: RestaurantList 안쓰는 import 제거(#33)
    
    * feat: RestaurantListItem 엔티티 및 리포지터리 추가(#33)
    
    * fix: restaurant_list_item 테이블명 단수(#33)

---
+++ Backend/src/main/java/com/whattoeat/domain/restaurantlist/repository/RestaurantListItemRepository.java
@@ -0,0 +1,7 @@
+package com.whattoeat.domain.restaurantlist.repository;
+
+import com.whattoeat.domain.restaurantlist.entity.RestaurantListItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantListItemRepository extends JpaRepository<RestaurantListItem, Long> {
+}
