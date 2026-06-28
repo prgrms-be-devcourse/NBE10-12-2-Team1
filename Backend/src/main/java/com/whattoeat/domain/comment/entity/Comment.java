a5c6dca3187e98152e37d01f7cfa330a19e58f1a
evelman2001-lgtm <evelman2001@gmail.com>
2026-06-29 08:56:52 +0900

    [feat] JWT Util 구현 (#39)
    * chore: 프로젝트 초기 설정(#1)
    
    * chore: 초기설정(#9)
    
    * feat: Comment엔티티 생성(#14)
    
    * chore: .idea 디렉토리 git 추적 제거(#24)
    
    * feat: JWT 유틸구현(#36)
    
    * feat: JWT 유틸구현(#36)
    
    * chore: git hooks 수정(#36)

--- Backend/src/main/java/com/whattoeat/domain/comment/entity/Comment.java
+++ Backend/src/main/java/com/whattoeat/domain/comment/entity/Comment.java
@@ -1,9 +1,13 @@
 package com.whattoeat.domain.comment.entity;
 
+import com.whattoeat.domain.feed.entity.Feed;
+import com.whattoeat.domain.user.entity.User;
 import com.whattoeat.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,9 +18,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Comment extends BaseEntity {
 
-    @JoinColumn(name="post_id", nullable = false)
-    private Post post;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id", nullable = false)
+    private Feed feed;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
