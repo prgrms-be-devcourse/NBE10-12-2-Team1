e8649c65504c3268679e865c711bfa614ff2cca2
SOL4R1S <mingh662@gmail.com>
2026-06-30 10:37:41 +0900

    feat: global/rq/Rq.java 생성 (#26)

---
+++ Backend/src/main/java/com/whattoeat/global/rq/Rq.java
@@ -0,0 +1,49 @@
+package com.whattoeat.global.rq;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class Rq {
+    private final HttpServletRequest request;
+    private final HttpServletResponse response;
+
+    public String getCookieValue(String name, String defaultValue) {
+        return Arrays.stream(Optional.ofNullable(request.getCookies()).orElse(new Cookie[0]))
+                .filter(cookie -> name.equals(cookie.getName()))
+                .map(Cookie::getValue)
+                .filter(value -> value != null && !value.isBlank())
+                .findFirst().orElse(defaultValue);
+    }
+
+    public String getCookieValue(String name) {
+        return getCookieValue(name, null);
+    }
+
+    public void setCookie(String name, String value, int maxAge) {
+        if (value == null) value = "";
+
+        Cookie cookie = new Cookie(name, value);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(value.isBlank() ? 0 : maxAge);
+        response.addCookie(cookie);
+    }
+
+    public void setCookie(String name, String value) {
+        setCookie(name, value, 60*60*24);
+    }
+
+    public void delCookie(String name) {
+        setCookie(name, null);
+    }
+
+}
