46d249f08a7157cce563934fdea72501cc919902
evelman2001-lgtm <evelman2001@gmail.com>
2026-06-30 15:37:01 +0900

    feat: Kakao Client 구현중(#70)

---
+++ Backend/src/main/java/com/whattoeat/domain/restaurant/dto/KakaoPlaceDocument.java
@@ -0,0 +1,24 @@
+package com.whattoeat.domain.restaurant.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class KakaoPlaceDocument {
+    @JsonProperty("address_name")
+    private String addressName;
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("phone")
+    private String phone;
+    @JsonProperty("road_address_name")
+    private String roadAddressName;
+    @JsonProperty("x")
+    private Double x;
+    @JsonProperty("y")
+    private Double y;
+    @JsonProperty("category_name")
+    private String categoryName;
+}
