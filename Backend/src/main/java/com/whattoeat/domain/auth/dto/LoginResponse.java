d8a4cf34edfc47c08e86062bb190275b414be372
SOL4R1S <mingh662@gmail.com>
2026-07-01 12:00:31 +0900

    chore: LoginResponse dto에 refreshtoken 추가 (#80)

--- Backend/src/main/java/com/whattoeat/domain/auth/dto/LoginResponse.java
+++ Backend/src/main/java/com/whattoeat/domain/auth/dto/LoginResponse.java
@@ -2,6 +2,7 @@ package com.whattoeat.domain.auth.dto;
 
 public record LoginResponse (
         String accessToken,
+        String refreshToken,
         String nickname,
         String profileImage
 )
