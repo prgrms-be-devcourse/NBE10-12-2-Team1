32ffe772cf5d0e0f8979c76be35b5473a7b9726d
SOL4R1S <mingh662@gmail.com>
2026-07-01 15:05:53 +0900

    chore: RestraurantList/ListItem Controller 수정(#80)

--- Backend/src/test/java/com/whattoeat/domain/restaurantlist/controller/RestaurantListItemControllerTest.java
+++ Backend/src/test/java/com/whattoeat/domain/restaurantlist/controller/RestaurantListItemControllerTest.java
@@ -27,6 +27,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -37,6 +38,7 @@ import org.springframework.test.web.servlet.MockMvc;
         controllers = RestaurantListController.class,
         excludeAutoConfiguration = SecurityAutoConfiguration.class
 )
+@AutoConfigureMockMvc(addFilters = false)
 class RestaurantListItemControllerTest {
 
     @Autowired
