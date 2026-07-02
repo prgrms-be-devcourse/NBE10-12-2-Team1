35dd593eb3bddd8e5ea0a9985d70f140599ff5d3
SOL4R1S <mingh662@gmail.com>
2026-07-02 15:04:57 +0900

    chore: LoginResponse 토큰 필드 제거 (#85)

--- Backend/src/main/java/com/whattoeat/domain/auth/dto/LoginResponse.java
+++ Backend/src/main/java/com/whattoeat/domain/auth/dto/LoginResponse.java
@@ -1,8 +1,6 @@
 package com.whattoeat.domain.auth.dto;
 
 public record LoginResponse (
-        String accessToken,
-        String refreshToken,
         String nickname,
         String profileImage
 )
