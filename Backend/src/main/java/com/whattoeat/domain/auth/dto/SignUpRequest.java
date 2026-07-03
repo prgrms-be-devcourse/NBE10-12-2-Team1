c891564fce3ecde76c3be3e6ad8fb0ffa245ad55
SOL4R1S <mingh662@gmail.com>
2026-07-03 11:24:41 +0900

    chore: SignUpRequest api명세서 반영 수정 (#85)

--- Backend/src/main/java/com/whattoeat/domain/auth/dto/SignUpRequest.java
+++ Backend/src/main/java/com/whattoeat/domain/auth/dto/SignUpRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 
 public record SignUpRequest (
     @NotBlank @Email @Size(min = 4, max =20) String loginId,
-    @NotBlank @Size(min = 4, max =20) String password,
+    @NotBlank @Size(min = 8, max =20) String password,
     @NotBlank @Size(max =20) String nickname
 )
 {}
