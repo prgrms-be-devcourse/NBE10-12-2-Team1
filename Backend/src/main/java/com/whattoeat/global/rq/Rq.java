89e24c4ca0840576ee57b61eb4be0abb8bd3542e
SOL4R1S <mingh662@gmail.com>
2026-07-03 11:36:20 +0900

    chore: Rq.java cookie.setSecure 수정 (#85)

--- Backend/src/main/java/com/whattoeat/global/rq/Rq.java
+++ Backend/src/main/java/com/whattoeat/global/rq/Rq.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -14,6 +15,9 @@ import java.util.Optional;
 public class Rq {
     private final HttpServletRequest request;
     private final HttpServletResponse response;
+
+    @Value("${cookie.secure:false}")
+    private boolean secure;
 
     public String getCookieValue(String name, String defaultValue) {
         return Arrays.stream(Optional.ofNullable(request.getCookies()).orElse(new Cookie[0]))
@@ -32,7 +36,7 @@ public class Rq {
 
         Cookie cookie = new Cookie(name, value);
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        cookie.setSecure(secure);
         cookie.setPath("/");
         cookie.setMaxAge(value.isBlank() ? 0 : maxAge);
         response.addCookie(cookie);
